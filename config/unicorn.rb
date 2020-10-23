3bb6e640535f5d8a02966e57874f6537489573de
wilromeo <wilfriedkp.romeo@mail.com>
2020-10-23 07:15:17 +0100

    nginx config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 require 'fileutils'
-preload_app true
-timeout 5
-worker_processes 4
-listen '/tmp/nginx.socket', backlog: 1024
+	preload_app true
+	timeout 5
+	worker_processes 4
+	listen '/tmp/nginx.socket', backlog: 1024
 
 before_fork do |server,worker|
 	FileUtils.touch('/tmp/app-initialized')
